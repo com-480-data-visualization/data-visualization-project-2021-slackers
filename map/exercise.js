5a9f7f8ca9e04ee41b3913b67933046ef747a2cc
lustea0201 <alexandre.luster@epfl.ch>
2021-05-12 21:05:54 +0200

    Actually make the button change shading, without reloading the data. Move to class.

--- map/exercise.js
+++ map/exercise.js
@@ -2,9 +2,10 @@ const highResolution = true;
 const WIDTH = 960;
 const HEIGHT = 500;
 const MIN_OBS = 10;
+const DEFAULTCOUNTRYCOLOR = "green";
 
 const projector = d3.geoNaturalEarth1(); /*geoNaturalEarth1, geoMercator, geopEquirectangular etc.*/
-const chosenTrait = "open";
+var chosenTrait = "none";
 
 const RES = highResolution ? 50 : 110;
 
@@ -30,135 +31,164 @@ function whenDocumentLoaded(action) {
   }
 }
 
-function makeWorldMap() {
-  /*Select Map SVG and set size*/
-  const svg = d3.select("svg");
-  svg.attr("width", WIDTH).attr("height", HEIGHT);
+var stats = {};
+var id_to_name = {};
+var id_to_isoa2 = {};
+var name_to_isoa2 = {};
 
-  /* Define the map projections*/
-  const height = +svg.attr("height");
-  const width = +svg.attr("width");
-  const projection = projector.scale(
-    Math.min(width / Math.PI, height / Math.PI)
-  );
-  const pathGenerator = d3.geoPath().projection(projection);
 
-  /* Allow zooming*/
-  const g = svg.append("g");
-  svg.call(
-    d3.zoom().on("zoom", () => {
-      g.attr("transform", d3.event.transform);
-    })
-  );
+class Map {
+	constructor (trait) {
+	  /*Select Map SVG and set size*/
+	  const svg = d3.select("svg");
+	  svg.attr("width", WIDTH).attr("height", HEIGHT);
 
-  /* Draw background earth (i.e. sea)*/
-  g.append("path")
-    .attr("class", "sphere")
-    .attr("d", pathGenerator({ type: "Sphere" }));
+	  /* Define the map projections*/
+	  const height = +svg.attr("height");
+	  const width = +svg.attr("width");
+	  const projection = projector.scale(
+	    Math.min(width / Math.PI, height / Math.PI)
+	  );
+	  const pathGenerator = d3.geoPath().projection(projection);
 
-  /* Load country info from world atlas, topojson file and our dataset, execute when all are loaded. */
-  Promise.all([
-    d3.tsv("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.tsv"),
-    d3.json("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.json"),
-    d3.csv("../data/clean_data.csv", rowConverter),
-  ]).then(([tsvData, topoJSONdata, csvData]) => {
-    /* Create helper objects to convert between full name, iso-alpha 2 name and code used in topjson*/
-    const id_to_name = {};
-    const id_to_isoa2 = {};
-    const name_to_isoa2 = {};
-    tsvData.forEach((d) => {
-      id_to_name[d.iso_n3] = d.name;
-      id_to_isoa2[d.iso_n3] = d.iso_a2;
-      name_to_isoa2[d.name] = d.iso_a2;
-    });
+	  /* Allow zooming*/
+	  const g = svg.append("g");
+	  svg.call(
+	    d3.zoom().on("zoom", () => {
+	      g.attr("transform", d3.event.transform);
+	    })
+	  );
+	  this.g = g;
+	  /* Draw background earth (i.e. sea)*/
+	  g.append("path")
+	    .attr("class", "sphere")
+	    .attr("d", pathGenerator({ type: "Sphere" }));
 
-    /* Compute stats (i.e. mean of each trait and count) for each country*/
-    var stats = {};
-    csvData.forEach((row) => {
-      const country = row.country;
-      if (!(country in stats)) {
-        stats[country] = {
-          count: 1,
-          agre: row.agre,
-          extr: row.extr,
-          open: row.open,
-          cons: row.cons,
-          neur: row.neur,
-        };
-      } else {
-        stats[country].count += 1;
-        stats[country].agre += row.agre;
-        stats[country].extr += row.extr;
-        stats[country].open += row.open;
-        stats[country].cons += row.cons;
-        stats[country].neur += row.neur;
-      }
-    });
-    /* Divide by total to make a mean, and keep track of min and max for the chosen trait (used for color scale)*/
-    var min = 1;
-    var max = 0;
+	  /* Load country info from world atlas, topojson file and our dataset, execute when all are loaded. */
+	  Promise.all([
+	    d3.tsv("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.tsv"),
+	    d3.json("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.json"),
+	    d3.csv("../data/clean_data.csv", rowConverter),
+	  ]).then(([tsvData, topoJSONdata, csvData]) => {
+	    /* Create helper objects to convert between full name, iso-alpha 2 name and code used in topjson*/
 
-    Object.keys(stats).forEach((key) => {
-      stats[key].agre /= stats[key].count;
-      stats[key].extr /= stats[key].count;
-      stats[key].open /= stats[key].count;
-      stats[key].cons /= stats[key].count;
-      stats[key].neur /= stats[key].count;
-      if (stats[key].count >= MIN_OBS) {
-        max = Math.max(max, stats[key][chosenTrait]);
-        min = Math.min(min, stats[key][chosenTrait]);
-      }
-    });
+	    tsvData.forEach((d) => {
+	      id_to_name[d.iso_n3] = d.name;
+	      id_to_isoa2[d.iso_n3] = d.iso_a2;
+	      name_to_isoa2[d.name] = d.iso_a2;
+	    });
 
-    const colorScale = d3
-      .scaleSequential(d3.interpolateRdYlGn)
-      .domain([min, max]);
+	    /* Compute stats (i.e. mean of each trait and count) for each country*/
 
-    /* Draw all topojson countries*/
-    const countries = topojson.feature(
-      topoJSONdata,
-      topoJSONdata.objects.countries
-    );
+	    csvData.forEach((row) => {
+	      const country = row.country;
+	      if (!(country in stats)) {
+	        stats[country] = {
+	          count: 1,
+	          agre: row.agre,
+	          extr: row.extr,
+	          open: row.open,
+	          cons: row.cons,
+	          neur: row.neur,
+	        };
+	      } else {
+	        stats[country].count += 1;
+	        stats[country].agre += row.agre;
+	        stats[country].extr += row.extr;
+	        stats[country].open += row.open;
+	        stats[country].cons += row.cons;
+	        stats[country].neur += row.neur;
+	      }
+	    });
+		Object.keys(stats).forEach((key) => {
+		  stats[key].agre /= stats[key].count;
+		  stats[key].extr /= stats[key].count;
+		  stats[key].open /= stats[key].count;
+		  stats[key].cons /= stats[key].count;
+		  stats[key].neur /= stats[key].count;
+		});
 
-    g.selectAll("path")
-      .data(countries.features)
-      .enter()
-      .append("path")
-      .attr("class", "country")
-      .attr("fill", (d) => {
-        var color;
-        try {
-          color = colorScale(stats[id_to_isoa2[d.id]][chosenTrait]);
-          const n_obs = stats[id_to_isoa2[d.id]].count;
-          if (n_obs < MIN_OBS) {
-            color = "lightgray";
-          }
-        } catch (e) {
-          console.log("Uncaught: ", id_to_isoa2[d.id]); /**/
-          color = "lightgray";
-        }
-        return color;
-      })
-      .attr("d", pathGenerator)
-      .append("title")
-      .text((d) => {
-        var title;
-        try {
-          title =
-            id_to_name[d.id] + " (" + stats[id_to_isoa2[d.id]].count + ")";
-        } catch (e) {
-          title = "Unidentified";
-        }
-        return title;
-      });
-  });
+		function colorFill(t) {
+			var min = 1;
+		    var max = 0;
+
+		    Object.keys(stats).forEach((key) => {
+		      if (stats[key].count >= MIN_OBS) {
+		        max = Math.max(max, stats[key][t]);
+		        min = Math.min(min, stats[key][t]);
+		      }
+		    });
+
+		    const colorScale = d3
+		      .scaleSequential(d3.interpolateRdYlGn)
+		      .domain([min, max]);
+
+
+			return (d) => {
+			  var color;
+			  try {
+				color = colorScale(stats[id_to_isoa2[d.id]][t]);
+				const n_obs = stats[id_to_isoa2[d.id]].count;
+				if (n_obs < MIN_OBS) {
+				  color = "lightgray";
+				}
+			  } catch (e) {
+				console.log("Uncaught: ", id_to_isoa2[d.id]); /**/
+				color = "lightgray";
+			  }
+			  return color;
+			}
+		}
+
+		this.colorFill = colorFill
+
+		/* Draw all topojson countries*/
+		const countries = topojson.feature(
+		  topoJSONdata,
+		  topoJSONdata.objects.countries
+		);
+		this.features = countries.features
+
+
+	    this.countries = g.selectAll("path")
+	      .data(this.features)
+	      .enter()
+	      .append("path")
+	      .attr("class", "country")
+		  .attr("fill", DEFAULTCOUNTRYCOLOR)
+	      .attr("d", pathGenerator)
+	      .append("title")
+	      .text((d) => {
+	        var title;
+	        try {
+	          title =
+	            id_to_name[d.id] + " (" + stats[id_to_isoa2[d.id]].count + ")";
+	        } catch (e) {
+	          title = "Unidentified";
+	        }
+	        return title;
+	      });
+
+	  });
+	}
+
+
 }
 
+var map;
+
 whenDocumentLoaded(() => {
-  makeWorldMap();
+  map = new Map(chosenTrait);
 });
 
+
+
 const selectElem = document.getElementById("traits");
-selectElem.onchange = function () {
-  alert("You changed the selection!");
+selectElem.onchange = function (d) {
+  chosenTrait = d3.select(this).property("value");
+  if (chosenTrait === "none") {
+	   map.g.selectAll("path").attr("fill", DEFAULTCOUNTRYCOLOR);
+  } else {
+	   map.g.selectAll("path").attr("fill", map.colorFill(chosenTrait));
+  }
 };
