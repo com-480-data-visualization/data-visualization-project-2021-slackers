22825b13c6ff8f1893d8c32d3bdf160538d8ea1a
Furkan Karakas <furkan.karakas@epfl.ch>
2021-05-26 18:55:47 +0300

    work

--- map/main.js
+++ map/main.js
@@ -49,193 +49,201 @@ var id_to_isoa2 = {};
 var name_to_isoa2 = {};
 
 class Map {
-	constructor (trait) {
-		 /*Select Map SVG and set size*/
-		 const svg = d3.select("#map");
-		 svg.attr("width", WIDTH).attr("height", HEIGHT);
+  constructor(trait) {
+    /*Select Map SVG and set size*/
+    const svg = d3.select("#map");
+    svg.attr("width", WIDTH).attr("height", HEIGHT);
 
-		 /* Define the map projections*/
-		 const height = +svg.attr("height");
-		 const width = +svg.attr("width");
-		 const projection = projector.scale(
-			 Math.min(width / Math.PI, height / Math.PI)
-		 );
-		 const pathGenerator = d3.geoPath().projection(projection);
+    /* Define the map projections*/
+    const height = +svg.attr("height");
+    const width = +svg.attr("width");
+    const projection = projector.scale(
+      Math.min(width / Math.PI, height / Math.PI)
+    );
+    const pathGenerator = d3.geoPath().projection(projection);
 
-		 /*Scale for age*/
-		 var xScale = d3.scaleLinear()
-		 	.domain([0, 100])         // This is what is written on the Axis: from 0 to 100
-			.range([0, WIDTH/2]);       // This is where the axis is placed: from 100px to 800px
-		// Draw the axis
-		var scaleSvg = d3.select("#ageScale").attr("width", WIDTH).attr("height", scaleH);
+    /*Scale for age*/
+    var xScale = d3
+      .scaleLinear()
+      .domain([0, 100]) // This is what is written on the Axis: from 0 to 100
+      .range([0, WIDTH / 2]); // This is where the axis is placed: from 100px to 800px
+    // Draw the axis
+    var scaleSvg = d3
+      .select("#ageScale")
+      .attr("width", WIDTH)
+      .attr("height", scaleH);
 
-		var ageAxis = scaleSvg
-			.append("g")
-			.attr("transform", `translate(250,${scaleH/2})`)      // This controls the vertical position of the Axis
-			.call(d3.axisBottom(xScale));
+    var ageAxis = scaleSvg
+      .append("g")
+      .attr("transform", `translate(250,${scaleH / 2})`) // This controls the vertical position of the Axis
+      .call(d3.axisBottom(xScale));
 
+    const brush = d3
+      .brushX()
+      .extent([
+        [0, 0],
+        [WIDTH / 2, 20],
+      ])
+      .on("brush end", function brushended() {
+        const ext = d3.brushSelection(this);
+        minAge = parseInt(xScale.invert(ext[0]));
+        maxAge = parseInt(xScale.invert(ext[1]));
+        d3.select("#ageTitle").text(`Selected age: ${minAge}-${maxAge}`);
+        if (chosenTrait !== "none") {
+          map.g
+            .selectAll("path")
+            .attr("fill", map.colorFill(chosenTrait, minAge, maxAge));
+        }
+      });
 
-		const brush = d3.brushX()
-			.extent( [ [0, 0], [WIDTH/2, 20] ] )
-			.on("brush end", function brushended() {
-				const ext = d3.brushSelection(this);
-				minAge = parseInt(xScale.invert(ext[0]));
-				maxAge = parseInt(xScale.invert(ext[1]));
-				d3.select("#ageTitle").text(`Selected age: ${minAge}-${maxAge}`);
-				if (chosenTrait !== "none") {
-					map.g.selectAll("path").attr("fill", map.colorFill(chosenTrait, minAge, maxAge));
-				}
-			});
+    ageAxis.call(brush);
 
-		ageAxis.call(brush);
+    scaleSvg
+      .append("text")
+      .attr("id", "ageTitle")
+      .attr("x", 10)
+      .attr("y", 20)
+      .text(`Selected age: ${minAge}-${maxAge}`);
 
-		scaleSvg.append("text").attr("id", "ageTitle").attr("x", 10).attr("y", 20).text(`Selected age: ${minAge}-${maxAge}`);
+    /* Allow zooming*/
+    const g = svg.append("g");
+    svg.call(
+      d3.zoom().on("zoom", () => {
+        g.attr("transform", d3.event.transform);
+      })
+    );
 
+    this.g = g;
+    /* Draw background earth (i.e. sea)*/
+    g.append("path")
+      .attr("class", "sphere")
+      .attr("d", pathGenerator({ type: "Sphere" }));
 
-		/* Allow zooming*/
-		const g = svg.append("g");
-		svg.call(
-			d3.zoom().on("zoom", () => {
-				g.attr("transform", d3.event.transform);
-			}));
+    /* Load country info from world atlas, topojson file and our dataset, execute when all are loaded. */
+    Promise.all([
+      d3.tsv("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.tsv"),
+      d3.json("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.json"),
+      d3.csv("../data/clean_data.csv", rowConverter),
+    ]).then(([tsvData, topoJSONdata, csvData]) => {
+      /* Create helper objects to convert between full name, iso-alpha 2 name and code used in topjson*/
 
-		this.g = g;
-		/* Draw background earth (i.e. sea)*/
-		g.append("path")
-	    	.attr("class", "sphere")
-	    	.attr("d", pathGenerator({ type: "Sphere" }));
+      tsvData.forEach((d) => {
+        id_to_name[d.iso_n3] = d.name;
+        id_to_isoa2[d.iso_n3] = d.iso_a2;
+        name_to_isoa2[d.name] = d.iso_a2;
+      });
 
+      /* Compute stats (i.e. mean of each trait and count) for each country*/
+      this.rawCSV = csvData;
+      this.computeStats = function () {
+        stats = {};
+        var data = this.rawCSV;
 
-		/* Load country info from world atlas, topojson file and our dataset, execute when all are loaded. */
-		Promise.all([
-	    	d3.tsv("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.tsv"),
-	    	d3.json("https://unpkg.com/world-atlas@1.1.4/world/" + RES + "m.json"),
-	    	d3.csv("../data/clean_data.csv", rowConverter)]).then(([tsvData, topoJSONdata, csvData]) => {
-	    /* Create helper objects to convert between full name, iso-alpha 2 name and code used in topjson*/
+        var byAge = crossfilter(map.rawCSV).dimension((d) => d.age);
+        data = byAge.filter([minAge, maxAge]).top(Infinity);
 
-	    tsvData.forEach((d) => {
-	      id_to_name[d.iso_n3] = d.name;
-	      id_to_isoa2[d.iso_n3] = d.iso_a2;
-	      name_to_isoa2[d.name] = d.iso_a2;
-	    });
+        if (chosenSex !== "Both") {
+          var byAge = crossfilter(map.rawCSV).dimension((d) => d.sex);
+          data = byAge.filter(chosenSex).top(Infinity);
+        }
+        console.log(data.length);
 
-	    /* Compute stats (i.e. mean of each trait and count) for each country*/
-		this.rawCSV = csvData;
-		this.computeStats = function() {
-			stats = {}
-			var data = this.rawCSV;
+        data.forEach((row) => {
+          const country = row.country;
+          if (!(country in stats)) {
+            stats[country] = {
+              count: 1,
+              agre: row.agre,
+              extr: row.extr,
+              open: row.open,
+              cons: row.cons,
+              neur: row.neur,
+            };
+          } else {
+            stats[country].count += 1;
+            stats[country].agre += row.agre;
+            stats[country].extr += row.extr;
+            stats[country].open += row.open;
+            stats[country].cons += row.cons;
+            stats[country].neur += row.neur;
+          }
+        });
+        Object.keys(stats).forEach((key) => {
+          stats[key].agre /= stats[key].count;
+          stats[key].extr /= stats[key].count;
+          stats[key].open /= stats[key].count;
+          stats[key].cons /= stats[key].count;
+          stats[key].neur /= stats[key].count;
+        });
 
+        return stats;
+      };
 
-			var byAge = crossfilter(map.rawCSV).dimension(d => d.age);
-			data = byAge.filter([minAge, maxAge]).top(Infinity);
+      this.colorFill = function () {
+        stats = this.computeStats();
 
-			if (chosenSex !== "Both") {
-				var byAge = crossfilter(map.rawCSV).dimension(d => d.sex);
-				data = byAge.filter(chosenSex).top(Infinity);
-			}
-			console.log(data.length)
+        var min = 1;
+        var max = 0;
 
+        Object.keys(stats).forEach((key) => {
+          if (stats[key].count >= MIN_OBS) {
+            max = Math.max(max, stats[key][chosenTrait]);
+            min = Math.min(min, stats[key][chosenTrait]);
+          }
+        });
 
-			data.forEach((row) => {
-			  const country = row.country;
-			  if (!(country in stats)) {
-				stats[country] = {
-				  count: 1,
-				  agre: row.agre,
-				  extr: row.extr,
-				  open: row.open,
-				  cons: row.cons,
-				  neur: row.neur,
-				};
-			  } else {
-				stats[country].count += 1;
-				stats[country].agre += row.agre;
-				stats[country].extr += row.extr;
-				stats[country].open += row.open;
-				stats[country].cons += row.cons;
-				stats[country].neur += row.neur;
-			  }
-			});
-			Object.keys(stats).forEach((key) => {
-			  stats[key].agre /= stats[key].count;
-			  stats[key].extr /= stats[key].count;
-			  stats[key].open /= stats[key].count;
-			  stats[key].cons /= stats[key].count;
-			  stats[key].neur /= stats[key].count;
-			});
+        const colorScale = d3
+          .scaleSequential(d3.interpolateRdYlGn)
+          .domain([min, max]);
 
-			return stats
-		}
+        return (d) => {
+          var color;
+          try {
+            color = colorScale(stats[id_to_isoa2[d.id]][chosenTrait]);
+            const n_obs = stats[id_to_isoa2[d.id]].count;
+            if (n_obs < MIN_OBS) {
+              color = "lightgray";
+            }
+          } catch (e) {
+            console.log("Uncaught: ", id_to_isoa2[d.id]); /**/
+            color = "lightgray";
+          }
+          return color;
+        };
+      };
 
+      /* Draw all topojson countries*/
+      const countries = topojson.feature(
+        topoJSONdata,
+        topoJSONdata.objects.countries
+      );
+      this.features = countries.features;
 
-
-
-		this.colorFill = function() {
-			stats = this.computeStats();
-
-			var min = 1;
-		    var max = 0;
-
-		    Object.keys(stats).forEach((key) => {
-		      if (stats[key].count >= MIN_OBS) {
-		        max = Math.max(max, stats[key][chosenTrait]);
-		        min = Math.min(min, stats[key][chosenTrait]);
-		      }
-		    });
-
-		    const colorScale = d3
-		      .scaleSequential(d3.interpolateRdYlGn)
-		      .domain([min, max]);
-
-
-			return (d) => {
-			  var color;
-			  try {
-				color = colorScale(stats[id_to_isoa2[d.id]][chosenTrait]);
-				const n_obs = stats[id_to_isoa2[d.id]].count;
-				if (n_obs < MIN_OBS) {
-				  color = "lightgray";
-				}
-			  } catch (e) {
-				console.log("Uncaught: ", id_to_isoa2[d.id]); /**/
-				color = "lightgray";
-			  }
-			  return color;
-			}
-		}
-
-
-		/* Draw all topojson countries*/
-		const countries = topojson.feature(
-		  topoJSONdata,
-		  topoJSONdata.objects.countries
-		);
-		this.features = countries.features
-
-
-	    this.countries = g.selectAll("path")
-	      .data(this.features)
-	      .enter()
-	      .append("path")
-	      .attr("class", "country")
-		  .attr("fill", DEFAULTCOUNTRYCOLOR)
-	      .attr("d", pathGenerator)
-	      .append("title")
-	      .text((d) => {
-	        var title;
-	        try {
-	          title =
-	            id_to_name[d.id] + " (" + stats[id_to_isoa2[d.id]].count + ")";
-	        } catch (e) {
-	          title = "Unidentified";
-	        }
-	        return title;
-	      });
-
-	  });
-	}
-
-
+      this.countries = g
+        .selectAll("path")
+        .data(this.features)
+        .enter()
+        .append("path")
+        .attr("class", "country")
+        .on("click", () => {
+          // TODO: Add a histogram if clicked
+          alert("hi :)");
+        })
+        .attr("fill", DEFAULTCOUNTRYCOLOR)
+        .attr("d", pathGenerator)
+        .append("title")
+        .text((d) => {
+          var title;
+          try {
+            title =
+              id_to_name[d.id] + " (" + stats[id_to_isoa2[d.id]].count + ")";
+          } catch (e) {
+            title = "Unidentified";
+          }
+          return title;
+        });
+    });
+  }
 }
 
 var map;
@@ -244,7 +252,7 @@ whenDocumentLoaded(() => {
   map = new Map(chosenTrait);
 });
 
-const selectElem = document.getElementById("traits");
+const selectElem = document.getElementById("traits-div");
 
 selectElem.onchange = function (d) {
   chosenTrait = d3.select(this).property("value");
