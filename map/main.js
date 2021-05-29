31346c8bdd1fee34c6776b21197134d6c1dfe769
Furkan Karakas <furkan.karakas@epfl.ch>
2021-05-30 00:59:12 +0300

    add histogram not working

--- map/main.js
+++ map/main.js
@@ -290,6 +290,97 @@ class Map {
         };
       };
 
+      this.addHistogram = function (e) {
+        let data = this.rawCSV;
+        const byCountry = crossfilter(data).dimension((d) => d.country);
+        data = byCountry.filter(id_to_isoa2[e["id"]]).top(Infinity);
+        //const byTrait = crossfilter(data).dimension((d) => d.agre);
+        //data = byCountry.filter(byTrait).top(Infinity);
+
+        const agreScores = [];
+        const ages = [];
+
+        data.forEach((row) => {
+          agreScores.push(row["agre"]);
+          ages.push(row["age"]);
+        });
+
+        data = {
+          x0: ages,
+          length: agreScores,
+        };
+
+        console.log(data);
+
+        // set the dimensions and margins of the graph
+        var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+          width = 460 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
+
+        // append the svg object to the body of the page
+        var svg = d3
+          .select("#histogram")
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr(
+            "transform",
+            "translate(" + margin.left + "," + margin.top + ")"
+          );
+
+        // X axis: scale and draw:
+        var x = d3
+          .scaleLinear()
+          .domain([0, 99]) // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+          .range([0, width]);
+        svg
+          .append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+        // set the parameters for the histogram
+        var histogram = d3
+          .histogram()
+          .value(function (d) {
+            return d.price;
+          }) // I need to give the vector of value
+          .domain(x.domain()) // then the domain of the graphic
+          .thresholds(x.ticks(70)); // then the numbers of bins
+
+        // And apply this function to data to get the bins
+        var bins = histogram(data);
+
+        // Y axis: scale and draw:
+        var y = d3.scaleLinear().range([height, 0]);
+        y.domain([
+          0,
+          d3.max(bins, function (d) {
+            return d.length;
+          }),
+        ]); // d3.hist has to be called before the Y axis obviously
+        svg.append("g").call(d3.axisLeft(y));
+
+        // append the bar rectangles to the svg element
+        svg
+          .selectAll("rect")
+          .data(bins)
+          .enter()
+          .append("rect")
+          .attr("x", 1)
+          .attr("transform", function (d) {
+            return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+          })
+          .attr("width", function (d) {
+            console.log(d);
+            return x(d.x1) - x(d.x0);
+          })
+          .attr("height", function (d) {
+            return height - y(d.length);
+          })
+          .style("fill", "#69b3a2");
+      };
+
       /* Draw all topojson countries*/
       const countries = topojson.feature(
         topoJSONdata,
@@ -305,9 +396,8 @@ class Map {
         .enter()
         .append("path")
         .attr("class", "country")
-        .on("click", () => {
-          // TODO: Add a histogram if clicked
-          alert("hi :)");
+        .on("click", (e) => {
+          this.addHistogram(e);
         })
         .attr("fill", DEFAULTCOUNTRYCOLOR)
         .attr("d", pathGenerator)
